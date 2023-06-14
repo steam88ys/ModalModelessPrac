278e793846fd2306bfd173bf9dcc21df047e41bd
Yunseo Kim <s2102@e-mirim.hs.kr>
2023-06-14 11:38:53 +0900

    다양한 메시지박스 옵션 실습

--- ModalModelessPrac/Program.cs
+++ ModalModelessPrac/Program.cs
@@ -1,22 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
-namespace ModalModelessPrac
+namespace ModalModelessPrac31
 {
-    static class Program
+    public partial class Form1 : Form
     {
-        /// <summary>
-        /// 해당 애플리케이션의 주 진입점입니다.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        public Form1()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("내용");
+            MessageBox.Show("내용", "제목");
+            DialogResult result;
+            do
+            {
+                result = MessageBox.Show("내용", "제목", MessageBoxButtons.RetryCancel);
+            } while (result == DialogResult.Retry);
+            if (result == DialogResult.Cancel)
+            {
+                MessageBox.Show("사용자가 취소하였습니다.", "알림",
+                    MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
         }
     }
 }
